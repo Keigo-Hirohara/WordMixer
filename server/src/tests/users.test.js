c384ddc6092acdf9e1bebe70e8e9d297a1bcebb7
hirokei-camel <hirokei.camel@gmail.com>
2022-03-23 20:27:08 +0900

    テストコードに登録機能を追加しました

--- server/src/tests/users.test.js
+++ server/src/tests/users.test.js
@@ -5,6 +5,19 @@ const request = require("supertest");
 const app = require("../app");
 
 const usersTest = () => {
+  describe("Test POST /register", () => {
+    const completeUserData = {
+      username: "keigo",
+      password: "158281872keigo",
+    };
+    test("It should respond with 200 success", async () => {
+      const response = await request(app)
+        .post("/v1/register")
+        .send(completeUserData)
+        .expect("Content-Type", /json/)
+        .expect(201);
+    });
+  });
   describe("Test POST /login", () => {
     const completeUserData = {
       username: "keigo",
